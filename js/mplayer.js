a9d5063b90679608625ae6dfba102184fa0c2ab5
sysmiyatake <miyatakehtml@gmail.com>
2024-12-02 01:27:12 +0900

    Update mplayer.js

--- js/mplayer.js
+++ js/mplayer.js
@@ -119,4 +119,13 @@ document.addEventListener("DOMContentLoaded", function () {
   updateTime();
 });
 
+  if (audio) {
+    if (audio.readyState >= 1) {
+      updateTime();
+    } else {
+      audio.addEventListener('loadedmetadata', () => {
+        updateTime();
+      });
+    }
+  }
 // "Sys. T. Player (beta)" Sys. T. Miyatake, (Last modified: Dec 02, 2024;)
